8856144519e2e2bcf9ac2d452929e9c9165d9886
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-07-27 00:12:44 +0800

    * dashboard welcome

--- payslip_generation_system/views/dashboard.py
+++ payslip_generation_system/views/dashboard.py
@@ -2,18 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.db import connection
 from django.http import JsonResponse
+from datetime import datetime
+from payslip_generation_system.models import UserRole 
 
 from django.contrib.auth.decorators import login_required
-from payslip_generation_system.models import UserRole 
 
 @login_required
 def dashboard(request):
     user = request.user
-    username = user.username  # Get the username
+    username = user.username
     user_role = request.session.get('role')
-    
+    current_datetime = datetime.now()  # gets current date and time
+
     return render(request, 'dashboard/index.html', {
         'user_role': user_role,
         'username': username,
+        'current_datetime': current_datetime,
     })
-
