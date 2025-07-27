5b187a6e5d82f7030c3db5b9ff0752faaa2266cb
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-07-27 15:34:24 +0800

    # Dashboard Finalization

--- payslip_generation_system/views/dashboard.py
+++ payslip_generation_system/views/dashboard.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login as auth_login, logout as aut
 from django.db import connection
 from django.http import JsonResponse
 from datetime import datetime
-from payslip_generation_system.models import UserRole 
+from payslip_generation_system.models import UserRole, Employee  
 
 from django.contrib.auth.decorators import login_required
 
@@ -12,10 +12,12 @@ def dashboard(request):
     user = request.user
     username = user.username
     user_role = request.session.get('role')
-    current_datetime = datetime.now()  # gets current date and time
+    current_datetime = datetime.now()
+    employee_count = Employee.objects.count()
 
     return render(request, 'dashboard/index.html', {
         'user_role': user_role,
         'username': username,
         'current_datetime': current_datetime,
+        'employee_count': employee_count
     })
