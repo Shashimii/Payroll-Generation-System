a1189d7095b725015965e9244d06f6951f82a055
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-07-29 10:56:35 +0800

    # Format Roles for display and fix the visibility of cards base on roles

--- payslip_generation_system/context_processors.py
+++ payslip_generation_system/context_processors.py
@@ -6,6 +6,7 @@ def global_user_context(request):
     username = request.user.username
     user_role = request.session.get('role', '')
     current_datetime = datetime.now()
+
     restricted_roles = [
         'employee',
         'preparator_meo_s',
@@ -13,6 +14,19 @@ def global_user_context(request):
         'preparator_meo_w',
         'preparator_meo_n'
     ]
+
+    ROLE_FORMAT = {
+        'admin': 'System Admin',
+        'checker': 'Checker',
+        'preparator_denr_ncr': 'Preparator: DENR NCR NEC',
+        'preparator_meo_s': 'Preparator: MEO South',
+        'preparator_meo_e': 'Preparator: MEO East',
+        'preparator_meo_w': 'Preparator: MEO West',
+        'preparator_meo_n': 'Preparator: MEO North',
+        'employee': 'Employee'
+    }
+
+    formatted_role = ROLE_FORMAT.get(user_role)
 
     try:
         if request.user.is_authenticated:
@@ -24,6 +38,7 @@ def global_user_context(request):
     return {
         'username': username,
         'user_role': user_role,
+        'formatted_user_role': formatted_role,
         'current_datetime': current_datetime,
         'restricted_roles': restricted_roles
     }
