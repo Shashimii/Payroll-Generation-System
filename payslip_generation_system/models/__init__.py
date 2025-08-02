2da94c626ade66f2a7f0c7c215fc2f31fadded27
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-08-02 18:29:54 +0800

    # Added Batch Assignment db table model

--- payslip_generation_system/models/__init__.py
+++ payslip_generation_system/models/__init__.py
@@ -2,3 +2,4 @@ from .user_role import UserRole
 from .employee import Employee
 from .employee import EmployeeAttachment
 from .adjustment import Adjustment
+from .batch_assignment import BatchAssignment
---
+++ payslip_generation_system/models/batch_assignment.py
@@ -0,0 +1,44 @@
+from django.db import models
+from .employee import Employee
+
+class BatchAssignment(models.Model):
+    employee = models.ForeignKey(
+        Employee,
+        on_delete=models.CASCADE,
+        related_name='batchassignment'
+    )
+
+    batch_number = models.IntegerField()
+
+    CUTOFF_MONTH_CHOICES = [
+        ('January', 'January'),
+        ('February', 'February'),
+        ('March', 'March'),
+        ('April', 'April'),
+        ('May', 'May'),
+        ('June', 'June'),
+        ('July', 'July'),
+        ('August', 'August'),
+        ('September', 'September'),
+        ('October', 'October'),
+        ('November', 'November'),
+        ('December', 'December'),
+    ]
+
+    cutoff_month = models.CharField(max_length=20, choices=CUTOFF_MONTH_CHOICES)
+
+    CUTOFF_PERIOD_CHOICES = [
+        ('1st', '1st'), 
+        ('2nd', '2nd')
+    ]
+
+    cutoff = models.CharField(max_length=10, choices=CUTOFF_PERIOD_CHOICES)
+
+    cutoff_year = models.CharField(max_length=50)  
+
+    class Meta:
+        unique_together = ['employee']
+        ordering = ['cutoff_year', 'cutoff_month', 'cutoff', 'batch_number']
+    
+    def __str__(self):
+        return f"{self.employee.fullname} - Batch {self.batch_number} ({self.cutoff_month} {self.cutoff}, {self.cutoff_year})"
