aa50be7079e8284ea906c48bbcc4daafc9a0f2dd
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-08-13 00:28:34 +0800

    # register the factory

--- payslip_generation_system/factories/__init__.py
+++ payslip_generation_system/factories/__init__.py
@@ -1,3 +1,4 @@
 # Declare factories here
 # include this on the seeder => from payslip_generation_system.factories import EmployeeFactory
 from .employee import EmployeeFactory
+from .batch import BatchFactory
